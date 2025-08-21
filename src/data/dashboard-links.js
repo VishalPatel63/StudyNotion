a753124dcb7d9ce02ed746a597d3baf991798e07
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2025-08-21 20:24:03 +0530

    Update dashboard-links.js

--- src/data/dashboard-links.js
+++ src/data/dashboard-links.js
@@ -36,9 +36,10 @@ export const sidebarLinks = [
   },
   {
     id: 6,
-    name: "Purchase History",
+    name: "Add to cart",
     path: "/dashboard/cart",
     type: ACCOUNT_TYPE.STUDENT,
     icon: "VscHistory",
   },
 ];
+
